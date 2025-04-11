8ccdd47b73ec3a71a6a1b91e68e91e39927de371
Julien <julien.sabathe@ynov.com>
2025-04-11 18:09:20 +0200

    [ADD] Class Offer

---
+++ models.py
@@ -0,0 +1,11 @@
+class Offer:
+    def __init__(
+        self, offer_id, title, location, dept_code, url, company_name, description
+    ):
+        self.id = offer_id
+        self.title = title
+        self.location = location
+        self.dept_code = dept_code
+        self.url = url
+        self.company_name = company_name
+        self.description = description
